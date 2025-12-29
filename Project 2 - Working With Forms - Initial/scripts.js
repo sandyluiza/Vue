5da842b1bfcffbec638e7895784ff224778e767c
Sandy Luiza Martins de Oliveira <sandy@engepack.com.br>
2025-12-29 15:13:50 -0300

    Aprendendo um pouco mais

---
+++ Project 2 - Working With Forms - Initial/scripts.js
@@ -0,0 +1,39 @@
+members = [
+    {
+        fname: 'John',
+        lname: 'Lennon',
+        instrument: 'Violão'
+    },
+    {
+        fname: 'George',
+        lname: 'Harrison',
+        instrument: 'Guitarra'
+    }
+]
+
+const handlingForms = {
+
+    data() {
+        return {
+            members: window.members,
+            newMember: {
+               
+            }
+        }
+    },
+
+    methods: {
+        addMember: function() {
+            if (this.newMember.fname && this.newMember.lname && this.newMember.instrument) {
+                this.members.push(this.newMember);
+                this.newMember = {};
+            }else{
+                alert("Por favor, preencha todos os campos antes de adicionar um novo membro.");
+            }
+
+        }
+    }
+
+};
+
+Vue.createApp(handlingForms).mount('#app');
